0d2a1bb91c1e6eb465506c98f4f3484d610c6b03
Joaquin Ruiz <70347742+JoacoR7@users.noreply.github.com>
2023-07-24 11:47:37 -0300

    Prueba deploy

--- configuration/db.py
+++ configuration/db.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
+import env
 
-engine = create_engine("mysql+pymysql://root:root@localhost:3306/project")
+# Generar la cadena de conexión usando las variables de entorno definidas en env.py
+db_url = f"mysql+pymysql://{env.DB_USER}:{env.DB_PASSWORD}@{env.DB_HOST}:{env.DB_PORT}/{env.DB_NAME}"
+
+# Crear el motor de la base de datos
+engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 conn = SessionLocal()
 meta = MetaData()
